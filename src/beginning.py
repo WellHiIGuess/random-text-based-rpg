0280615d699d4c20a5561c50f40d8b167315571a
WellHiIGuess <67477071+WellHiIGuess@users.noreply.github.com>
2023-07-06 20:35:48 -0400

    Made beginning speech

--- src/beginning.py
+++ src/beginning.py
@@ -1,2 +1,4 @@
 def beginning():
+    print('\033c', end='')
+    print('This land used to be a land of peace. Now it is frought with peril an monsters. The royals no longer care about their people instead they throw the people to the monsters as sacrifices. But from this peril will come heros, warriors who will rise up. You must never lose hope. Take your rightfull place to the throne even it is kills you!')
 
